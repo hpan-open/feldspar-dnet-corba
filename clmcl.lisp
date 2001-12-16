55c49e704953c9e97dbea872bcdb2019e5c51d7a
lenst <lenst>
2001-12-16 17:45:59 +0000

    *** empty log message ***

--- clmcl.lisp
+++ clmcl.lisp
@@ -1,20 +1,31 @@
 (in-package :cl-user)
 
-(load "clorb:clorb.system")
 (load "clorb:clorb-pkgdcl")
-(load "clorb:mcl-patches")
 
-(make:oos :clorb :load)
+(defparameter clorb:*port*  4711)
+(defparameter clorb::*name-service* "NameService")
+
+(defparameter *naming-base-path*
+  (make-pathname :directory '(:relative "naming")
+                 :name "foo"
+                 :type "obj"))
+
+(ensure-directories-exist *naming-base-path* :verbose t)
+
+(load "clorb:clorb-files")
+(clorb:reload)
+
+;;(make:oos :clorb :load)
 
 (import 'cl-user::§ :clorb)
-(import 'cl-user::§§ :clorb)
+(import 'cl-user::§§§ :clorb)
 (import 'cl-user::% :clorb)
 
 
-(define-symbol-macro cl-user::§§§
+(define-symbol-macro cl-user::§§
   (in-package :clorb))
 
-(define-symbol-macro clorb::§§§
+(define-symbol-macro clorb::§§
   (in-package :cl-user))
 
 
@@ -39,3 +50,25 @@
 
 (setq clorb::*host*
       (ccl::tcp-addr-to-str (ccl::local-interface-ip-address)))
+
+#+if-t2-has-ifr
+(with-open-stream (s (ccl::open-tcp-stream "172.17.17.18" 5111))
+  (let ((ior (read-line s)))
+    (when (and (stringp ior)
+               (> (length ior) 4)
+               (string= "IOR:" ior :end2 4))
+      (when (eql #\Linefeed (char ior (1- (length ior))))
+        (setq ior (subseq ior 0 (1- (length ior)))))
+      (setq clorb::*interface-repository* ior))))
+
+(with-open-stream (s (ccl::open-tcp-stream "172.17.17.1" 80))
+  (let ((crlf (coerce (vector #\Return #\Linefeed) 'string)))
+    (format s "GET /InterfaceRepository~A" crlf))
+  (let ((ior (read-line s)))
+    (when (and (stringp ior)
+               (> (length ior) 4)
+               (string= "IOR:" ior :end2 4))
+      (when (eql #\Linefeed (char ior (1- (length ior))))
+        (setq ior (subseq ior 0 (1- (length ior)))))
+      (setq clorb::*interface-repository* ior))))
+
