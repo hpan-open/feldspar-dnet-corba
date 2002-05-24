3a1b63b82ae28403e79b3686536131d86dc77b25
lenst <lenst>
2002-05-24 06:07:56 +0000

    Dispatch on the CORBA: classes, not on the local-ifr classes.

--- idef-write.lisp
+++ idef-write.lisp
@@ -8,7 +8,7 @@ This guides the use of local or absolute names.")
 
 
 
-(defmethod gen-iref ((pdef primitive-def))
+(defmethod gen-iref ((pdef CORBA:PrimitiveDef))
   (ecase (op:kind pdef)
     (:pk_void 'void)
     (:pk_short 'short)
@@ -30,15 +30,28 @@ This guides the use of local or absolute names.")
     (:pk_wchar 'wchar)
     (:pk_wstring 'wstring)))
 
-(defmethod gen-iref ((idef contained))
+(defmethod gen-iref ((idef CORBA:Contained))
   (if (eq (op:defined_in idef) *idef-write-container*)
       (op:name idef)
     (op:absolute_name idef)))
 
 
-(defmethod gen-iref ((seq sequence-def))
+(defmethod gen-iref ((seq CORBA:SequenceDef))
   `(sequence ,(gen-iref (op:element_type_def seq))
              ,(op:bound seq)))
+
+(defmethod default-repoid ((obj CORBA:Contained) &optional prefix)
+  (let ((names (list ":" (op:version obj))))
+    (loop for c = obj then (op:defined_in c)
+        while (typep c 'CORBA:Contained)
+        unless (eq c obj) do (push "/" names)
+        do (push (op:name c) names))
+    (when prefix
+      (push "/" names)
+      (push prefix names))
+    (apply 'concatenate 'string
+           "IDL:"
+           names)))
 
 (defun contained-id-info (obj)
   (append
@@ -48,13 +61,13 @@ This guides the use of local or absolute names.")
      (list :id (op:id obj)))))
 
 
-(defmethod gen-idef ((tdef alias-def))
+(defmethod gen-idef ((tdef CORBA:AliasDef))
   `(define-type ,(op:name tdef)
        ,(gen-iref (op:original_type_def tdef))))
 
 
 ;;    (define-attribute "name" string :readonly t)
-(defmethod gen-idef ((adef attribute-def))
+(defmethod gen-idef ((adef CORBA:AttributeDef))
   `(define-attribute ,(op:name adef)
        ,(gen-iref (op:type_def adef))
      ,@(if (eq (op:mode adef) :attr_readonly)
@@ -62,7 +75,7 @@ This guides the use of local or absolute names.")
      ,@ (contained-id-info adef)))
 
 
-(defmethod gen-idef ((odef operation-def))
+(defmethod gen-idef ((odef CORBA:OperationDef))
   `(define-operation ,(op:name odef)
        ,(map 'list (lambda (param)
                      (list (op:mode param)
@@ -75,7 +88,7 @@ This guides the use of local or absolute names.")
 
 
 
-(defmethod gen-idef ((idef interface-def))
+(defmethod gen-idef ((idef CORBA:InterfaceDef))
   (let ((*idef-write-container* idef))
     `(define-interface ,(op:name idef) 
          (
@@ -85,13 +98,13 @@ This guides the use of local or absolute names.")
           ,@(contained-id-info idef))
        ,@(map 'list 'gen-idef (op:contents idef :dk_all t)))))
 
-(defmethod gen-idef ((mdef module-def))
+(defmethod gen-idef ((mdef CORBA:ModuleDef))
   (let ((*idef-write-container* mdef))
     `(define-module ,(op:name mdef) 
          (,@(contained-id-info mdef))
        ,@(map 'list 'gen-idef (op:contents mdef :dk_all t)))))
 
-(defmethod gen-idef ((sdef struct-def))
+(defmethod gen-idef ((sdef CORBA:StructDef))
   `(define-struct ,(op:name sdef) 
        ,(map 'list 
           (lambda (smember)
@@ -100,12 +113,12 @@ This guides the use of local or absolute names.")
           (op:members sdef))
      ,@(contained-id-info sdef)))
 
-(defmethod gen-idef ((enum enum-def))
+(defmethod gen-idef ((enum CORBA:EnumDef))
   `(define-enum ,(op:name enum) 
        ,(coerce (op:members enum) 'list)
      ,@(contained-id-info enum)))
 
-(defmethod gen-idef ((union union-def))
+(defmethod gen-idef ((union CORBA:UnionDef))
   `(define-union ,(op:name union) 
        ,(gen-iref (op:discriminator_type_def union))
      ,(map 'list  
@@ -116,21 +129,25 @@ This guides the use of local or absolute names.")
         (op:members union))
      ,@ (contained-id-info union)))
 
-(defmethod gen-idef ((exception exception-def))
+(defmethod gen-idef ((exception CORBA:ExceptionDef))
   `(define-exception ,(op:name exception) 
        ,(map 'list  
           (lambda (smember)
             (list (struct-get smember :name)
                   (gen-iref (struct-get smember :type_def))))
           (op:members exception))
-     ,@ (contained-id-info exception)))
+     ,@(contained-id-info exception)))
 
+(defmethod gen-idef ((const CORBA:ConstantDef))
+  `(define-constant ,(op:name const) ,(gen-iref (op:type_def const))
+     ,(op:value const)
+     ,@(contained-id-info const)))
 
 (defun idef-write (obj &key default-prefix)
   (let ((*idef-write-default-prefix* default-prefix))
     (let ((idef 
            (typecase obj
-             (repository   
+             (CORBA:Repository
               (map 'list 'gen-idef (op:contents obj :dk_all t)))
              (cons
               (map 'list 'gen-idef obj))
--- target.lisp
+++ target.lisp
@@ -62,23 +62,35 @@
                      (cdr x)
                      (list x)))))
 
+(defun make-target-ensure-package (package target)
+  (let* ((package-name (package-name package))
+         (exports (loop for sym in (slot-value target 'symbols)
+                        when (string= package-name (package-name (symbol-package sym)))
+                        collect (symbol-name sym))))
+    `(ensure-corba-package ,package-name 
+                           :export ',exports)))
+
+(defun make-allways-eval (code)
+  `(eval-when (:compile-toplevel :load-toplevel :execute)
+     ,code))
+
 (defun make-idltype (symbol id typecode &rest forms)
   (make-progn
-   `((setf (get ',symbol 'ifr-id) ,id)
-     (setf (get ',symbol 'typecode) ,typecode)
+   `((set-symbol-ifr-id ',symbol ,id)
+     (set-symbol-typecode ',symbol ,typecode)
      ,@forms)))
 
-(defun target-base-list (target bases root-class)
-  (if bases 
+(defun target-base-list (target bases make-symbol-fun root-class)
+  (if (zerop (length bases))
+      `(,root-class)
       (map 'list (lambda (base)
-                   (scoped-target-symbol target base))
-           bases)
-      `(,root-class)))
+                   (funcall make-symbol-fun target base))
+           bases)))
 
 (defun target-defclass (target idef)
   (let ((bases (op:base_interfaces idef)))
-    `(defclass ,(scoped-target-symbol target idef) 
-      ,(target-base-list target bases 'CORBA:Object)
+    `(defclass ,(scoped-target-symbol target idef)
+      ,(target-base-list target bases #'scoped-target-symbol 'CORBA:Object)
        ())))
 
 (defun target-defproxyclass (target idef)
@@ -86,8 +98,9 @@
         (class-symbol (target-proxy-class-symbol target idef)))
     `(progn
        (defclass ,class-symbol
-           ,(target-base-list target bases
-                              (list (scoped-target-symbol target idef) 'CORBA:Proxy))
+         ,(list* (scoped-target-symbol target idef)
+                 (target-base-list target bases #'target-proxy-class-symbol
+                                   'CORBA:Proxy))
          ())
        (register-proxy-class ,(op:id idef) ',class-symbol))))
 
@@ -106,7 +119,7 @@
 (defun getter-name (name)
   (concatenate 'string "_get_" name))
 
-(defmethod target-type ((obj primitive-def) target)
+(defmethod target-type ((obj CORBA:PrimitiveDef) target)
   (declare (ignore target))
   (ecase (op:kind obj)
     (:pk_short 'CORBA:short)
@@ -129,65 +142,70 @@
     (:pk_wstring 'CORBA:wstring)))
 
 
-(defmethod target-type ((obj sequence-def) target)
+(defmethod target-type ((obj CORBA:SequenceDef) target)
   (declare (ignore target))
   `sequence)
 
-(defmethod target-type ((obj alias-def) target)
+(defmethod target-type ((obj CORBA:AliasDef) target)
   (scoped-target-symbol target obj))
 
-(defmethod target-typecode ((obj primitive-def) target)
+(defmethod target-typecode ((obj CORBA:PrimitiveDef) target)
   (declare (ignore target))
   (ecase (op:kind obj)
-   (:pk_any `'CORBA:tc_any)
-   (:pk_boolean `'CORBA:tc_boolean)
-   (:pk_char `'CORBA:tc_char)
-   (:pk_double `'CORBA:tc_double)
-   (:pk_float `'CORBA:tc_float)
-   (:pk_long `'CORBA:tc_long)
-   (:pk_longdouble `'CORBA:tc_longdouble)
-   (:pk_longlong `'CORBA:tc_longlong)
-   (:pk_objref `'CORBA:tc_objref)
-   (:pk_octet `'CORBA:tc_octet)
-   (:pk_short `'CORBA:tc_short)
-   (:pk_string `'CORBA:tc_string)
-   (:pk_typecode `'CORBA:tc_typecode)
-   (:pk_ulong `'CORBA:tc_ulong)
-   (:pk_ulonglong `'CORBA:tc_ulonglong)
-   (:pk_ushort `'CORBA:tc_ushort)
-   (:pk_wchar `'CORBA:tc_wchar)))
+   (:pk_any `CORBA:tc_any)
+   (:pk_boolean `CORBA:tc_boolean)
+   (:pk_char `CORBA:tc_char)
+   (:pk_double `CORBA:tc_double)
+   (:pk_float `CORBA:tc_float)
+   (:pk_long `CORBA:tc_long)
+   (:pk_longdouble `CORBA:tc_longdouble)
+   (:pk_longlong `CORBA:tc_longlong)
+   (:pk_objref `CORBA:tc_objref)
+   (:pk_octet `CORBA:tc_octet)
+   (:pk_short `CORBA:tc_short)
+   (:pk_string `CORBA:tc_string)
+   (:pk_typecode `CORBA:tc_typecode)
+   (:pk_ulong `CORBA:tc_ulong)
+   (:pk_ulonglong `CORBA:tc_ulonglong)
+   (:pk_ushort `CORBA:tc_ushort)
+   (:pk_wchar `CORBA:tc_wchar)))
 
 
-(defmethod target-typecode ((x sequence-def) target)
+(defmethod target-typecode ((x CORBA:SequenceDef) target)
   `(make-sequence-typecode 
     ,(target-typecode (op:element_type_def x) target)
     ,(op:bound x)))
 
 
-(defmethod target-typecode ((x idltype) target)
-  `(get ,(scoped-target-symbol target x) 'typecode))
+(defmethod target-typecode ((x CORBA:IDLType) target)
+  `(symbol-typecode ',(scoped-target-symbol target x)))
 
 
 ;;;; target-code methods
 
-(defmethod target-code ((x contained) target)
+(defmethod target-code ((x CORBA:Contained) target)
   (declare (ignore target))
   (mess 4 "Can't generate code for ~A of kind ~S"
         (op:name x)
         (op:def_kind x))
-  (describe x))
+  )
 
-(defmethod target-code ((x alias-def) target)
+(defmethod target-code ((x CORBA:AliasDef) target)
   (let ((symbol (scoped-target-symbol target x)))
-    (make-idltype 
+    (make-idltype
      symbol
      (op:id x)
-     `(make-tc-alias ,(op:id x) ,(op:name x)
-                     ,(target-typecode (op:original_type_def x) target))
-     `(deftype ,symbol
-               ',(target-type (op:original_type_def x) target)))))
+     `(lambda ()
+        (make-tc-alias ,(op:id x) ,(op:name x)
+                       ,(target-typecode (op:original_type_def x) target)))
+     `(deftype ,symbol ()
+        ',(target-type (op:original_type_def x) target)))))
 
-(defmethod target-code ((idef interface-def) target)
+(defmethod target-code ((const CORBA:ConstantDef) target)
+  `(defconstant ,(scoped-target-symbol target const)
+     ',(op:value const)))
+
+(defmethod target-code ((idef CORBA:InterfaceDef) target)
   (make-idltype
    (scoped-target-symbol target idef)
    (op:id idef)
@@ -198,7 +216,7 @@
     (map 'list (lambda (x) (target-code x target))
          (op:contents idef :dk_all t)))))
 
-(defmethod target-code ((op operation-def) target)
+(defmethod target-code ((op CORBA:OperationDef) target)
   (let* ((op-name (op:name op))
          (lisp-name (make-target-symbol target op-name :op))
          (class (target-proxy-class-symbol target (op:defined_in op))))
@@ -206,41 +224,49 @@
        (apply 'clorb::invoke obj ,op-name args))))
 
 
-(defmethod target-code ((op attribute-def) target)
+(defmethod target-code ((op CORBA:AttributeDef) target)
   (let* ((att-name (op:name op))
          (lisp-name (make-target-symbol target att-name :op))
          (class (target-proxy-class-symbol target (op:defined_in op))))
     `(progn
-       (defmethod ,lisp-name ((obj ,class) &rest)
-         (apply 'clorb::invoke obj ,(getter-name att-name)))
+       (defmethod ,lisp-name ((obj ,class) &rest args)
+         (apply 'clorb::invoke obj ,(getter-name att-name) args))
        ,@(if (eq (op:mode op) :attr_normal)
              (list `(defmethod (setf ,lisp-name) (newval (obj ,class))
                       (apply 'clorb::invoke obj ,(setter-name att-name)
                              newval)))))))
 
 
-(defmethod target-code ((mdef module-def) target)
+(defmethod target-code ((mdef CORBA:ModuleDef) target)
   (make-progn (map 'list (lambda (contained)
                            (target-code contained target))
                    (op:contents mdef :dk_All t))))
 
-(defmethod target-code ((sdef struct-def) target)
-  `(define-corba-struct ,(scoped-target-symbol target sdef)
-       :id ,(op:id sdef)
-       :members ,(map 'list 
-                  (lambda (smember)
-                    (list (make-target-symbol target (op:name smember)
-                                             'clorb)
-                          nil))
-                  (op:members sdef))))
+(defmethod target-code ((sdef CORBA:StructDef) target)
+  (make-idltype 
+   (scoped-target-symbol target sdef)
+   (op:id sdef)
+   `(lambda ()
+      (struct-typecode ,(op:id sdef) ,(op:name sdef)
+                       ,@(loop for smember in (coerce (op:members sdef) 'list)
+                               nconc (list (op:name smember)
+                                           (target-typecode (op:type_def smember) target)))))
+   `(define-corba-struct ,(scoped-target-symbol target sdef)
+      :id ,(op:id sdef)
+      :members ,(map 'list 
+                     (lambda (smember)
+                       (list (make-target-symbol target (op:name smember)
+                                                 'clorb)
+                             nil))
+                     (op:members sdef)))))
 
-(defmethod target-code ((enum enum-def) target)
+(defmethod target-code ((enum CORBA:EnumDef) target)
   `(deftype ,(scoped-target-symbol target enum) ()
      '(member ,@(map 'list (lambda (name) (make-target-symbol target name :keyword))
                      (op:members enum)))))
 
-(defmethod target-code ((exc exception-def) target)
-  `(define-user-exception ,(scoped-target-symbol target exc)
+(defmethod target-code ((exc CORBA:ExceptionDef) target)
+  `(Define-user-exception ,(scoped-target-symbol target exc)
        :id ,(op:id exc)
        :slots ,(map 'list
                  (lambda (smember)
@@ -252,14 +278,55 @@
 
 ;;;; Stub code generator
 
-(defun stub-code (name)
+(defparameter *stub-code-ignored-packages*
+  (list (find-package :keyword)
+        (find-package :clorb)
+        (find-package :op)))
+
+(defun stub-code (object)
+  (when (stringp object)
+    (setq object (lookup-name object)))
   (let ((target (make-instance 'code-target)))
-    (let ((code (target-code (lookup-name name) target))
-          (ignored-packages (list (find-package :keyword)
-                                  (find-package :clorb)
-                                  (find-package :op))))
+    (let ((code (target-code object target))
+          (ignored-packages *stub-code-ignored-packages*))
       `(,@(loop for package in (slot-value target 'packages)
                 unless (member package ignored-packages)
                 collect `(defpackage ,(package-name package) (:use)))
         (export (,@(slot-value target 'symbols)))
         ,code))))
+
+(defun gen-stub-file (object filename &key package-def)
+  (with-open-file (*standard-output* filename :direction :output :if-exists :supersede)
+    (let* ((target (make-instance 'code-target))
+           (code (target-code object target)))
+      (print '(in-package :clorb))
+      (when package-def
+        (loop for package in (slot-value target 'packages)
+                unless (member package *stub-code-ignored-packages*)
+                do (terpri)
+                   (pprint (make-allways-eval 
+                            (make-target-ensure-package package target)))))
+      (mapc (lambda (x) (terpri) (pprint x))
+            (remove nil (cdr code)))
+      (terpri))))
+
+
+#|
+(load "clorb:src;iop-idl")
+(gen-stub-file (lookup-name "IOP") "clorb:x-iop.lisp")
+(gen-stub-file (lookup-name "CORBA") "clorb:orb-stub.lisp")
+(gen-stub-file (lookup-name "CosNaming") "clorb:x-cosnaming.lisp" :package-def t)
+
+
+(defvar *target* (make-instance 'code-target))
+(target-code (lookup-name "CosNaming") *target*)
+
+(pprint
+(loop for sym in (slot-value *target* 'symbols)
+      collect (cons (package-name (symbol-package sym)) (symbol-name sym))))
+
+(pprint (make-target-ensure-package (elt (slot-value *target* 'packages) 2) *target*))
+
+
+
+|#
