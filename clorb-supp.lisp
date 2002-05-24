4bb5436ef4f68a9db9785f698f657dc611df24aa
lenst <lenst>
2002-05-24 06:06:58 +0000

    New fun ensure-corba-package to be used in generated code.

--- clorb-supp.lisp
+++ clorb-supp.lisp
@@ -1,5 +1,5 @@
 ;;;; clorb-supp.lisp
-;; $Id: clorb-supp.lisp,v 1.5 2002/03/21 19:31:12 lenst Exp $
+;; $Id: clorb-supp.lisp,v 1.6 2002/05/24 10:06:58 lenst Exp $
 
 (in-package :clorb)
 
@@ -23,6 +23,14 @@
           (princ (code-char octet) stream)
         (format stream "<~x>" octet)))
     oid))
+
+
+(defun ensure-corba-package (name &key nicknames export)
+  (let ((package (find-package name)))
+    (unless package
+      (setq package (make-package name :nicknames nicknames :use '())))
+    (export (mapcar (lambda (sym-name) (intern sym-name package)) export)
+            package)))
 
 
 ;;; clorb-supp.lisp ends here
