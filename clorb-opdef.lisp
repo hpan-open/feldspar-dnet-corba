4696653e0c9a559d096ff102c3a00b1024d165d1
lenst <lenst>
2002-06-01 01:44:54 +0000

    code clean out

--- clorb-opdef.lisp
+++ clorb-opdef.lisp
@@ -10,45 +10,28 @@
 
 ;;(define-slot-dumper opdef)
 
+#|
 (defmethod op:type :before ((s CORBA:ParameterDescription) &rest x)
   (declare (ignore x))
   (unless (slot-value s 'type)
     (setf (slot-value s 'type)
       (op:type (op:type_def s)))))
-
-
-(setf (get :in 'param-mode) :param_in) 
-(setf (get :out 'param-mode) :param_out)
-(setf (get :inout 'param-mode) :param_inout)
+|#
 
 (defun make-param (name mode typecode)
-  (unless (typep mode 'CORBA::ParameterMode)
-    (setq mode (get mode 'param-mode))
-    (assert mode))
-  (make-instance 'CORBA::ParameterDescription
-    :name name
-    :type typecode
-    :mode mode))
-
-(defun param-name (param)
-  (op:name param))
-
-(defun param-mode (param)
-  (op:mode param))
-
-(defun param-typecode (param)
-  (op:type param))
+  (check-type mode CORBA::ParameterMode)
+  (CORBA:ParameterDescription :name name :type typecode :mode mode))
 
 (defun opdef-params-with-mode (opdef modes)
   (loop for param in (opdef-params opdef)
-        when (member (param-mode param) modes)
+        when (member (op:mode param) modes)
         collect param))
 
 (defun opdef-inparams (opdef)
   (opdef-params-with-mode opdef '(:param_in :param_inout)))
 
 (defmethod opdef-inparam-typecodes ((opdef opdef))
-  (mapcar #'param-typecode (opdef-inparams opdef)))
+  (mapcar #'op:type (opdef-inparams opdef)))
 
 (defun opdef-outparams (opdef)
   (nconc
@@ -58,4 +41,4 @@
    (opdef-params-with-mode opdef '(:param_out :param_inout))))
 
 (defmethod opdef-outparam-typecodes ((opdef opdef))
-  (mapcar #'param-typecode (opdef-outparams opdef)))
+  (mapcar #'op:type (opdef-outparams opdef)))
