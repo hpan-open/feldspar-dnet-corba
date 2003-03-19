df520ab9c0ae2f0566b2a60ae3a94e5733670302
lenst <lenst>
2003-03-19 14:14:01 +0000

    *** empty log message ***

---
+++ clcmu.lisp
@@ -0,0 +1,18 @@
+(in-package :cl-user)
+
+(load "clorb-pkgdcl")
+(load "clorb-files")
+(clorb:reload)
+
+(setq clorb:*port* 5711)
+(setq clorb:*host* "localhost")
+
+(defvar *orb* (CORBA:ORB_init))
+
+(format t "~&;;; Activating the POA~%")
+(op:activate (op:the_poamanager (clorb::root-poa)))
+(format t "~&;;; ORB listening on port ~A~%" (clorb::orb-port clorb::*the-orb*))
+
+(defun hh ()
+  (setup-hello :file "hello.ior")
+  (hello-client :file "hello.ior"))
