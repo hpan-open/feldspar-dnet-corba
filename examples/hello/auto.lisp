91852dd7eb7bb12b1b872b84558ccce10dca9696
lenst <lenst>
2003-09-23 12:52:35 +0000

    *** empty log message ***

--- examples/hello/auto.lisp
+++ examples/hello/auto.lisp
@@ -3,7 +3,10 @@
 (defun hh (&key (name nil) (file "hello.ior"))
   (unless (and (find-package "HELLO")
                (find-symbol "WORLD" "HELLO"))
-    (load "clorb:examples;hello;loadup"))
+    (load (load-time-value
+           (make-pathname :name "loadup" :type nil
+                          :defaults *load-pathname*))))
+
   (funcall 'setup-hello :file file :name name)
   (funcall 'hello-client :file file :name name))
 
