ba42ecaaf8d55ce153575d79156d3caf4f37b8a0
lenst <lenst>
2004-01-14 12:11:37 +0000

    *** empty log message ***

--- test-client-request.lisp
+++ test-client-request.lisp
@@ -2,9 +2,7 @@
 
 (define-test-suite "Client Request"
 
-
   (define-test "one way static call"
-    
     (let ((obj (make-instance 'CORBA:Proxy
                  :id "IDL:test:1.0"
                  :profiles (list (make-iiop-profile
@@ -12,18 +10,21 @@
                                   :host "localhost"
                                   :port 9999
                                   :key #()))))
-          (conn (make-instance 'connection)))
+          (conn (make-instance 'connection
+                  :io-descriptor (make-io-descriptor
+                                  :shortcut-p t
+                                  :stream (make-instance 'shortcut-stream
+                                            :output (make-instance 'octets-stream))))))
       
       (setf (selected-profile obj) (first (object-profiles obj)))
       (setf (object-connection obj) conn)
-      
-      (static-call ("foo" obj)
-                   :output ((buffer) (marshal-long 123 buffer))
-                   :no-response t)
-      
-      )
-    
-    
-    )
+      (let ((*io-event-queue* nil))
+        (static-call ("foo" obj)
+                     :output ((buffer) (marshal-long 123 buffer))
+                     :no-response t))
+      (ensure 
+       (> (io-descriptor-write-pos (connection-io-descriptor conn)) 12))))
+  
 
-)
+
+#| end suite |# )
