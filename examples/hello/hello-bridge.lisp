cb2c9841c25b8a7a08d3ad1a4df1f6c06c051637
lenst <lenst>
2004-02-03 12:17:51 +0000

    example improve

--- examples/hello/hello-bridge.lisp
+++ examples/hello/hello-bridge.lisp
@@ -7,6 +7,22 @@
 (corba:define-method greet ((self hello-world-bridge))
   (format nil "Bridge: '~A'" (op:greet (other self))))
 
+(corba:define-method read ((self hello-world-bridge) name)
+  (let ((fox (op:read (other self) name)))
+    (when fox
+      (incf (op:value fox)))
+    fox))
+
+(corba:define-method write ((self hello-world-bridge) box)
+  (op:write (other self) box))
+
+(corba:define-method repr ((self hello-world-bridge) box)
+  (op:repr (other self) box))
+
+(corba:define-method write2 ((self hello-world-bridge) box1 box2)
+  (op:write2 (other self) box1 box2))
+
+
 ;;(defvar *hello-servant* nil)
 
 (setq *hello-servant*
