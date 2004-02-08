59b4968da05be433db81225a1e6d5fe32ea59510
lenst <lenst>
2004-02-08 15:43:28 +0000

    added generation of full interface description

--- clorb-ifr-info.lisp
+++ clorb-ifr-info.lisp
@@ -1,6 +1,6 @@
 (in-package :clorb)
 
-  
+
 (defun symbol-ifr-parent-id (symbol)
   (let ((parent (get symbol 'ifr-parent)))
     (if parent (symbol-ifr-id parent) "")))
@@ -11,6 +11,33 @@
    :id (op:id tc)
    :defined_in (symbol-ifr-parent-id symbol)
    :type tc))
+
+(defun ifr-kind (symbol)
+  (case (get-properties (symbol-plist symbol) '(ifr-result ifr-type ifr-bases))
+    (ifr-result :dk_operation)
+    (ifr-type   :dk_attribute)
+    (ifr-bases  :dk_interface)))
+
+(defmethod generate-ifr-description ((tc objref-typecode) symbol)
+  (let (opdesc atdesc)
+    (loop for sym in (get symbol 'ifr-contents)
+          for kind = (ifr-kind sym)
+          when (eq kind :dk_operation)
+          collect (ifr-description sym) into ops
+          when (eq kind :dk_attribute)
+          collect (ifr-description sym) into ats
+          finally (setq opdesc ops
+                        atdesc ats))
+    (corba:interfacedef/fullinterfacedescription
+     :name (get symbol 'ifr-name)
+     :id   (symbol-ifr-id symbol)
+     :defined_in ""
+     :version (get symbol 'ifr-version "1.0")
+     :operations opdesc
+     :attributes atdesc
+     :base_interfaces (mapcar #'symbol-ifr-id (get symbol 'ifr-bases)) 
+     :type (symbol-typecode symbol))))
+
 
 (defmethod generate-ifr-description ((tc null) symbol)
   ;; For Contained that hasn't got a TypeCode: 
