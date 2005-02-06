fce84be33639dac0f78d06bfedc9110ed71f6388
lenst <lenst>
2005-02-06 17:30:54 +0000

    *** empty log message ***

--- clorb-ifr-info.lisp
+++ clorb-ifr-info.lisp
@@ -5,18 +5,24 @@
   (let ((parent (get symbol 'ifr-parent)))
     (if parent (symbol-ifr-id parent) "")))
 
-(defmethod generate-ifr-description ((tc except-typecode) symbol)
-  (CORBA:EXCEPTIONDESCRIPTION
-   :name (op:name tc)
-   :id (op:id tc)
-   :defined_in (symbol-ifr-parent-id symbol)
-   :type tc))
+(defun ifr-version (symbol)
+  (get symbol 'ifr-version "1.0"))
 
 (defun ifr-kind (symbol)
   (case (get-properties (symbol-plist symbol) '(ifr-result ifr-type ifr-bases))
     (ifr-result :dk_operation)
     (ifr-type   :dk_attribute)
     (ifr-bases  :dk_interface)))
+
+
+(defmethod generate-ifr-description ((tc except-typecode) symbol)
+  (corba:exceptiondescription
+   :name (op:name tc)
+   :id (op:id tc)
+   :version (ifr-version symbol)
+   :defined_in (symbol-ifr-parent-id symbol)
+   :type tc))
+
 
 (defmethod generate-ifr-description ((tc objref-typecode) symbol)
   (let (opdesc atdesc)
@@ -29,10 +35,10 @@
           finally (setq opdesc ops
                         atdesc ats))
     (corba:interfacedef/fullinterfacedescription
-     :name (get symbol 'ifr-name)
+     :name (or (get symbol 'ifr-name) (op:name tc))
      :id   (symbol-ifr-id symbol)
      :defined_in ""
-     :version (get symbol 'ifr-version "1.0")
+     :version (ifr-version symbol)
      :operations opdesc
      :attributes atdesc
      :base_interfaces (mapcar #'symbol-ifr-id (get symbol 'ifr-bases)) 
@@ -41,7 +47,7 @@
 
 (defmethod generate-ifr-description ((tc null) symbol)
   ;; For Contained that hasn't got a TypeCode: 
-  ;;  InterfaceDef, ModuleDef, OperationDef, AttributeDef
+  ;;  ModuleDef, OperationDef, AttributeDef
   (multiple-value-bind (indicator value)
                        (get-properties (symbol-plist symbol) '(ifr-result ifr-type))
     (case indicator
@@ -49,7 +55,7 @@
        (CORBA:OperationDescription
         :id (symbol-ifr-id symbol)
         :name (get symbol 'ifr-name)
-        :version (get symbol 'ifr-version "1.0")
+        :version (ifr-version symbol)
         :defined_in (symbol-ifr-parent-id symbol)
         :result value
         :mode (get symbol 'ifr-mode)
@@ -63,13 +69,90 @@
        (CORBA:AttributeDescription
         :id (symbol-ifr-id symbol)
         :name (get symbol 'ifr-name)
-        :version (get symbol 'ifr-version "1.0")
+        :version (ifr-version symbol)
         :defined_in (symbol-ifr-parent-id symbol)
         :mode (get symbol 'ifr-mode)
         :type value))
       ((nil) 
        (error "Can't generate a description for ~S" symbol)))))
   
+
+
+
+
+
+;;;; interface ExceptionDef : Contained, Container
+;;;  readonly attribute TypeCode type;
+;;;  attribute StructMemberSeq members;
+
+(defclass desc-exceptiondef (CORBA:ExceptionDef)
+  ((desc  :initarg :description  :accessor description)))
+
+#| #<EXCEPTIONDESCRIPTION
+    :NAME "InvalidTypeForEncoding"
+    :ID "IDL:omg.org/IOP/Codec/InvalidTypeForEncoding:1.0"
+    :DEFINED_IN "IDL:omg.org/IOP/Codec:1.0"
+    :VERSION "1.0"
+    :TYPE #<EXCEPT-TYPECODE "InvalidTypeForEncoding" #x651B106>>
+|#
+
+;;; readonly attribute TypeCode type;
+(define-method op:type ((obj desc-exceptiondef)) (op:type (description obj)))
+;;; attribute StructMemberSeq members;
+(define-method op:members
+  ((obj desc-exceptiondef))
+  (let* ((tc (op:type (description obj)))
+         (n  (op:member_count tc)))
+    (loop for i from 0 below n
+         collect (let ((name (op:member_name tc i))
+                       (mtc  (op:member_type tc i)))
+                   (CORBA:StructMember
+                    :name name
+                    :type mtc
+                    :type_def nil)))))
+
+
+;;; CONTAINED
+;;; // read/write interface
+;;; attribute RepositoryId id;
+(define-method op:id ((obj desc-exceptiondef)) (op:id (description obj)))
+;;; attribute Identifier name;
+(define-method op:name ((obj desc-exceptiondef)) (op:name (description obj)))
+;;; attribute VersionSpec version;
+(define-method op:version ((obj desc-exceptiondef)) (op:version (description obj)))
+;;; // read interface
+;;; readonly attribute Container defined_in;
+;;; readonly attribute ScopedName absolute_name;
+;;; readonly attribute Repository containing_repository;
+;;; struct Description {
+;;;   DefinitionKind kind;
+;;;   any value;
+;;; };
+;;; Description describe ();
+(define-method op:describe
+  ((obj desc-exceptiondef))
+  (corba:contained/description
+   :kind :dk_exception
+   :value (description obj)))
+
+
+;;; CONTAINER
+;;; Contained lookup(in ScopedName search_name);
+;;; ContainedSeq contents(in DefinitionKind limit_type,
+;;;                       in boolean exclude_inherited);
+;;; ContainedSeq lookup_name(in Identifier search_name,
+;;;                          in long levels_to_search,
+;;;                          in DefinitionKind limit_type,
+;;;                          in boolean exclude_inherited );
+;;; struct Description {
+;;;   Contained contained_object;
+;;;   DefinitionKind kind;
+;;;   any value;
+;;; };
+;;; typedef sequence<Description> DescriptionSeq;
+;;; DescriptionSeq describe_contents(in DefinitionKind limit_type,
+;;;                                  in boolean exclude_inherited,
+;;;                                  in long max_returned_objs);
 
 
 #|
@@ -89,3 +172,5 @@
 
 
 |#
+
+
