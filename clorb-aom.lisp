d555676d90d461d87febb1a1aa0c6189ef108afa
lenst <lenst>
2005-02-21 13:09:54 +0000

    Allow for mutation while mapping

--- clorb-aom.lisp
+++ clorb-aom.lisp
@@ -20,7 +20,7 @@
     :accessor servant-activation-hash)))
 
 (defmethod servant-active-p ((aom object-map) servant)
-  (gethash servant (servant-activation-hash aom)))
+  (not (null (gethash servant (servant-activation-hash aom)))))
 
 (defmethod servant-oid-list ((aom object-map) servant)
   (mapcar #'car (gethash servant (servant-activation-hash aom))))
@@ -54,10 +54,23 @@
           ((null (cdr activations)) (caar activations))
           (t t))))
 
-(defmethod map-activations ((aom object-map) func)
-  (loop for activation-list being the hash-values of (servant-activation-hash aom)
-        do (loop for (oid . servant) in activation-list
-                 do (funcall func oid servant))))
+
+(defmethod map-activations ((aom object-map) func &optional mutable)
+  (cond (mutable
+         (let ((servant-hash (servant-activation-hash aom)))
+           (loop for servant
+              in (loop for servant being the hash-keys of servant-hash
+                    collect servant)
+              do (loop for activation
+                    in (copy-list (gethash servant servant-hash))
+                    when (member activation (gethash servant servant-hash))
+                    do (funcall func (car activation) (cdr activation))))))
+        (t
+         (loop for activation-list
+            being the hash-values of (servant-activation-hash aom)
+            do (loop for (oid . servant) in activation-list
+                  do (funcall func oid servant)))) ))
+
 
 (defmethod activation-count ((aom object-map))
   (dict-count (oid-activation-trie aom)))
